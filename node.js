7447112c61a6f3b4627c1361667bf66129670837
khaibt <khaibt.nhatnam@gmail.com>
2023-02-01 12:05:07 +0700

    first commit

---
+++ node.js
@@ -0,0 +1,20 @@
+
+
+let str = "123456789";
+
+
+// length
+// document.write(len);
+// alert(len);
+// console.log(len);
+
+//slice
+
+// let sli = str.slice();
+// alert(sli);
+
+
+//sub
+
+let subs = str.substring(-4);
+alert(subs);
